42a1e655c684d126ca80568a5e6c071d3d0403d2
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-11-10 11:49:24 +0100

    Final commit. Complete challenge.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,31 @@
 import React from "react";
+import Card from "./Card";
+import contacts from "../contacts.js";
 
 function App() {
-  return (
-    <div>
-      <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2>Beyonce</h2>
-          <img
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
-          />
+    return (
+        <div>
+            <h1 className="heading">My Contacts</h1>
+            <Card
+                name={contacts[0].name}
+                imgURL={contacts[0].imgURL}
+                phone={contacts[0].phone}
+                email={contacts[0].email}/>
+
+            <Card
+                name={contacts[1].name}
+                imgURL={contacts[1].imgURL}
+                phone={contacts[1].phone}
+                email={contacts[1].email}/>
+
+            <Card
+                name={contacts[2].name}
+                imgURL={contacts[2].imgURL}
+                phone={contacts[2].phone}
+                email={contacts[2].email}/>
+
         </div>
-        <div className="bottom">
-          <p>+123 456 789</p>
-          <p>b@beyonce.com</p>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
